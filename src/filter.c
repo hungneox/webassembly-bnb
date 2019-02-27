0fd59213fb2ffe98c9cceb72d6cfdbb978ffed5d
Hung Neo <hungnq1989@gmail.com>
2019-02-27 18:18:48 +0200

    Add Makefile, simplify filter.c

--- src/filter.c
+++ src/filter.c
@@ -8,15 +8,12 @@ void to_grayscale(unsigned char *buffer_in, unsigned char *buffer_out, unsigned 
     {
         for (int x = 0; x < (int)width; ++x)
         {
-            int redIndex = (y * width + x) * 4;
-            int greenIndex = redIndex + 1;
-            int blueIndex = redIndex + 2;
-            int alphaIndex = redIndex + 3;
-            float average = (buffer_in[redIndex] + buffer_in[greenIndex] + buffer_in[blueIndex]) / 3;
-            buffer_out[redIndex] = average;
-            buffer_out[greenIndex] = average;
-            buffer_out[blueIndex] = average;
-            buffer_out[alphaIndex] = 255;
+            int index = (y * width + x) * 4;
+            int average = (int)(buffer_in[index] + buffer_in[index + 1] + buffer_in[index + 2]) / 3;
+            buffer_out[index] = average;
+            buffer_out[index + 1] = average;
+            buffer_out[index + 2] = average;
+            buffer_out[index + 3] = 255;
         }
     }
 }
